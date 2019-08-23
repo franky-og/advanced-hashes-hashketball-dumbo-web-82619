c53473d0aa899c8a69c34d6a7867dda6395915f5
Franky Liu <cuifranky95@gmail.com>
2019-08-23 17:29:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,6 +138,8 @@ end
 def num_points_scored(player_name)
    hashball = game_hash
    if hashball[player_name] == true
-     hashball[player_name][points]
+     return hashball[player_name][points]
+   end
+ end
   
 
