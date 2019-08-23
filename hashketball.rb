b3156dd80fd5f26222ce236af466c38ebde7a5c3
Franky Liu <cuifranky95@gmail.com>
2019-08-23 18:19:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,7 +16,7 @@ def game_hash
       :blocks => 1,
       :slam_dunks => 1
     },
-    :name => "Reggie Evans" => {
+    { :name => "Reggie Evans", 
       :number => 30,
       :shoe => 14,
       :points => 12,
