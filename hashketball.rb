01748845e451834cd96aa1fc9374e58ee8905813
Franky Liu <cuifranky95@gmail.com>
2019-08-23 16:35:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -122,7 +122,7 @@ end
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    binding.pry
+    #binding.pry
     team_data.each do |attribute, data|
       #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
       binding.pry
