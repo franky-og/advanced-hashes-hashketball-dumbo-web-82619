da4d778cc7ca10d5772a5bdf1dc1aa6b7b4bcb00
Franky Liu <cuifranky95@gmail.com>
2019-08-23 16:01:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-# Write your code here!
+require "pry"  # Write your code here!
 def game_hash {
 
 :home => {
@@ -74,44 +74,44 @@ def game_hash {
       :slam_dunks => 2
     },
     "Bismack Biyombo" => {
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
+      :number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
     },
-    "" => {
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
+    "Desagna Diop" => {
+      :number => 2,
+      :shoe => 14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4.
+      :blocks => 5.
+      :slam_dunks => 5
     },
-    "" => {
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
+    "Ben Gordon" => {
+      :number => 8,
+      :shoe => 15,
+      :points => 33,
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
+      :slam_dunks => 0
     },
-    "" => {
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
+    "Kemba Walker" => {
+      :number => 33,
+      :shoe => 15,
+      :points => 6,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 7,
+      :blocks => 5,
+      :slam_dunks => 12
     },
   ]
 }
