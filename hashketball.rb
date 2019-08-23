04f9ff630bf953b745fd7f1b0c02bf881ec7ac7b
Franky Liu <cuifranky95@gmail.com>
2019-08-23 19:27:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
   :team_name => "Brooklyn Nets",
   :colors => ["Black", "White"],
   :players => [
-    { :name => "Alan Anderson",
+    "Alan Anderson" => {
       :number => 0,
       :shoe => 16,
       :points => 22,
@@ -16,7 +16,7 @@ def game_hash
       :blocks => 1,
       :slam_dunks => 1
     },
-    { :name => "Reggie Evans", 
+    "Reggie Evans" => {
       :number => 30,
       :shoe => 14,
       :points => 12,
