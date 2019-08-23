a43ca86bbfb2e3ed41777f271cb9d72ea57fee8c
Franky Liu <cuifranky95@gmail.com>
2019-08-23 09:31:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,39 @@
 # Write your code here!
+def game_hash {
+
+:home => {
+  :team_name => "Brooklyn Nets"
+  :color => ["Black", "White"]
+  :players => [
+    "" => {
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    },
+  ]
+    
+  
+}
+:away => {
+   :team_name => "Charlotte Hornets"
+  :color => ["Turquoise", "Purple"]
+  :players => [
+    :number =>
+    :shoe =>
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks
+    ]
+ 
+}
 
 
 
