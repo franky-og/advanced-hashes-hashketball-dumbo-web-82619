28089b6c79e284bbdffc7d34d5f5a77a17906b09
Franky Liu <cuifranky95@gmail.com>
2019-08-23 15:57:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,18 +63,58 @@ def game_hash {
    :team_name => "Charlotte Hornets"
   :color => ["Turquoise", "Purple"]
   :players => [
-    :number =>
-    :shoe =>
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks
-    ]
- 
+    "Jeff Adrien" => {
+      :number => 4,
+      :shoe => 18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2
+    },
+    "Bismack Biyombo" => {
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    },
+    "" => {
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    },
+    "" => {
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    },
+    "" => {
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    },
+  ]
 }
-
 
 
 
